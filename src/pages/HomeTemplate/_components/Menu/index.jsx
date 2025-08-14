70bfa8f8ff60f81ecccda1a2c9fe657da2d8eeb9
thanhlv-vietry <vinhthanhluong@gmail.com>
2025-08-14 17:29:51 +0700

    update movie-managerment popup

--- src/pages/HomeTemplate/_components/Menu/index.jsx
+++ src/pages/HomeTemplate/_components/Menu/index.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Menu() {
   return (
-    <ul className="flex flex-col p-4 lg:p-0 mt-4 font-bold border border-gray-100 rounded-lg bg-gray-50 lg:space-x-4 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
+    <ul className="flex flex-col p-4 lg:p-0 mt-4 font-bold border border-gray-100 rounded-lg bg-gray-50 lg:space-x-4 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-white">
       <li>
         <NavLink
           to="/"
